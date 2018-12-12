3050f34ffc5d635391e5ad87e666d88a91309ff4
Denis Sinkevich <sleepoker@gmail.com>
2018-12-12 22:46:06 +0300

    Add database properties
    Create DatasourceConfiguration class, SpringConfig refactor.

---
+++ src/main/java/by/sinkevich/config/DatasourceConfiguration.java
@@ -0,0 +1,66 @@
+package by.sinkevich.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource(value = "classpath:payments.online.database.properties")
+public class DatasourceConfiguration {
+
+	@Value("${datasource.driver}")
+	private String driverClassName;
+
+	@Value("${datasource.url}")
+	private String url;
+
+	@Value("${datasource.username}")
+	private String username;
+
+	@Value("${datasource.password}")
+	private String password;
+
+	@Value("${datasource.connection.useSSL}")
+	private String useSSL;
+
+	@Value("${datasource.connection.allowPublicKeyRetrieval}")
+	private String allowPublicKeyRetrieval;
+
+	@Value("${datasource.connection.useUnicode}")
+	private String useUnicode;
+
+	@Value("${datasource.connection.characterEncoding}")
+	private String characterEncoding;
+
+	public String getDriverClassName() {
+		return driverClassName;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getUseSSL() {
+		return useSSL;
+	}
+
+	public String getAllowPublicKeyRetrieval() {
+		return allowPublicKeyRetrieval;
+	}
+
+	public String getUseUnicode() {
+		return useUnicode;
+	}
+
+	public String getCharacterEncoding() {
+		return characterEncoding;
+	}
+}
--- src/main/java/by/sinkevich/config/SpringConfig.java
+++ src/main/java/by/sinkevich/config/SpringConfig.java
@@ -1,8 +1,6 @@
 package by.sinkevich.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -11,6 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
@@ -19,16 +18,30 @@ import javax.sql.DataSource;
 		"by.sinkevich.mapper",
 		"by.sinkevich.service.impl"
 })
+@Import(DatasourceConfiguration.class)
 public class SpringConfig {
+
+	@Bean
+	DatasourceConfiguration datasourceConfiguration() {
+		return new DatasourceConfiguration();
+	}
 
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/payments?" +
-				"useSSL=false&allowPublicKeyRetrieval=true&useUnicode=true&characterEncoding=utf8");
-		dataSource.setUsername("root");
-		dataSource.setPassword("pass");
+		dataSource.setDriverClassName(datasourceConfiguration().getDriverClassName());
+		dataSource.setUrl(datasourceConfiguration().getUrl());
+		dataSource.setUsername(datasourceConfiguration().getUsername());
+		dataSource.setPassword(datasourceConfiguration().getPassword());
+
+		Properties connectionProperties = new Properties();
+		connectionProperties.put("useSSL", datasourceConfiguration().getUseSSL());
+		connectionProperties.put("allowPublicKeyRetrieval",
+				datasourceConfiguration().getAllowPublicKeyRetrieval());
+		connectionProperties.put("useUnicode", datasourceConfiguration().getUseUnicode());
+		connectionProperties.put("characterEncoding",
+				datasourceConfiguration().getCharacterEncoding());
+		dataSource.setConnectionProperties(connectionProperties);
 		return dataSource;
 	}
 
